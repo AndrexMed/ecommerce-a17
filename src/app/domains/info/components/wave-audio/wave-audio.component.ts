61e1111d3b77d984f2599de00d742312a15d43f1
AndrexMed <giovanniandrex@gmail.com>
2023-11-19 18:35:38 -0500

    Creando la página "about us" o "conócenos"

--- src/app/domains/info/components/wave-audio/wave-audio.component.ts
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import WaveSurfer from 'wavesurfer.js';
 
@@ -14,10 +14,19 @@ export class WaveAudioComponent {
   @Input({ required: true }) audioUrl!: string;
   @ViewChild('wave') container!: ElementRef;
 
+  private ws!:  WaveSurfer;
+  isPlaying = signal(false);
+
   ngAfterViewInit() {
-    WaveSurfer.create({
+    this.ws = WaveSurfer.create({
       url: this.audioUrl,
       container: this.container.nativeElement
-    })
+    });
+    this.ws.on('play', () => this.isPlaying.set(true));
+    this.ws.on('pause', () => this.isPlaying.set(false));
+  }
+
+  playPause() {
+    this.ws.playPause();
   }
 }
