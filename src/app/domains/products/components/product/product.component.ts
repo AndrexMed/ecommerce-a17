7be26ef6c6e8944a461e69a8a102bfe84e1504fc
GiovanniAlzate <galzate@cesde.edu.co>
2023-11-20 09:57:50 -0500

    Construyendo tu propio pipe

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../../shared/models/product.model';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReversePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
---
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeAgo',
+  standalone: true
+})
+export class TimeAgoPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
