77e1e094c29850ea5c554f11c723dfae624f8e89
AndrexMed <giovanniandrex@gmail.com>
2023-11-19 20:24:56 -0500

    Creando el carrito de compras

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,11 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  hideSideMenu = signal(true);
+
+  toggleSideMenu(){
+    this.hideSideMenu.update(prevState => !prevState);
+  }
 
 }
